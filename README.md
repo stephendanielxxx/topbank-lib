e8b8572ebef269567ce4e14114438415cd3c0aa8
stephendanielxxx <stephendanielxxx@gmail.com>
2023-05-23 13:31:04 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# topbank-lib
---
+++ src/main/java/com/digimaster/jwt/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.digimaster.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+
+public class JwtUtil {
+
+    public Claims getClaims(final String token) {
+        try {
+            SecretKey secret = Keys.hmacShaKeyFor(Decoders.BASE64.decode(SecretUtils.JWT_SECRET));
+            Claims body = Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token).getBody();
+//            Claims body = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+            return body;
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " => " + e);
+        }
+        return null;
+    }
+
+    public void validateToken(final String token) throws JwtTokenMalformedException, JwtTokenMissingException {
+        try {
+            Jwts.parser().setSigningKey(SecretUtils.JWT_SECRET).parseClaimsJws(token);
+        } catch (SignatureException ex) {
+            throw new JwtTokenMalformedException("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            throw new JwtTokenMalformedException("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            throw new JwtTokenMalformedException("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            throw new JwtTokenMalformedException("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            throw new JwtTokenMissingException("JWT claims string is empty.");
+        }
+    }
+
+    public void validateRefreshToken(final String refreshToken) throws JwtTokenMalformedException, JwtTokenMissingException {
+        try {
+            Jwts.parser().setSigningKey(SecretUtils.JWT_SECRET_REFRESH).parseClaimsJws(refreshToken);
+        } catch (SignatureException ex) {
+            throw new JwtTokenMalformedException("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            throw new JwtTokenMalformedException("Invalid JWT refreshToken");
+        } catch (ExpiredJwtException ex) {
+            throw new JwtTokenMalformedException("Expired JWT refreshToken");
+        } catch (UnsupportedJwtException ex) {
+            throw new JwtTokenMalformedException("Unsupported JWT refreshToken");
+        } catch (IllegalArgumentException ex) {
+            throw new JwtTokenMissingException("JWT claims string is empty.");
+        }
+    }
+
+}
